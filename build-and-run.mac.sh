578da224445b07560d976b0dcdccf8193c11dfaa
Vo Hoan Vu <vohoanvu96@gmail.com>
2025-04-20 22:11:57 +0700

    dotnet build scripts for mac

---
+++ build-and-run.mac.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+echo "Building..."
+dotnet build SharedEntities/SharedEntities.csproj
+dotnet build FighterManager.Server/FighterManager.Server.csproj
+dotnet build VideoSharing.Server/VideoSharing.Server.csproj
+dotnet build MatchMaker.Server/MatchMaker.Server.csproj
+
+echo "Running backend services in background..."
+dotnet run --project FighterManager.Server/FighterManager.Server.csproj --launch-profile https &
+dotnet run --project VideoSharing.Server/VideoSharing.Server.csproj --launch-profile https &
+dotnet run --project MatchMaker.Server/MatchMaker.Server.csproj --launch-profile https &
+
+echo "All services started. Access at:"
+echo "FighterManager: http://localhost:7191/swagger"
+echo "VideoSharing: http://localhost:7192/swagger"
+echo "MatchMaker: http://localhost:7193/swagger"
+echo "React App: https://localhost:5173"
